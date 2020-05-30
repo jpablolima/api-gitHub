a3f7e65101e3b0e300c6663c09f717bce0967be4
João Pablo <jp.pablolima@gmail.com>
2020-05-30 12:38:10 -0300

    importando rotas no App

---
+++ src/home.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+import './App.css';
+
+function App(props) {
+  const [usuario, setUsuario] = useState('');
+  
+  function handlePesquisa(){
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+    
+  }
+  
+  return (
+      
+    <>
+     <input Classname = 'usuarioInput'  placeholder='Usuário' value={usuario} onChange={e => setUsuario(e.target.value)}/>
+      <button type='button' onClick= {handlePesquisa}>Pesquisar</button>
+    </>
+     
+  );
+}
+
+export default App;
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import repositories from './repositories';
 
-import repositories from './repositories';
+import Repositories from './Repositories';
+import home from './home';
 
 export default function Routes (){
   return(
     <BrowserRouter>
       <Switch>
-        <Route path='/repositories' component={repositories}/>
+        <Route path='/' exact component={home} />
+        <Route path='/repositories' component={Repositories}/>
       </Switch>
     </BrowserRouter>
   )
