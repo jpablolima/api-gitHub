99b83d62763c26e31e2ea666f6078b0224a69bb1
João Pablo <jp.pablolima@gmail.com>
2020-05-30 12:09:42 -0300

    consumindo dados da API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
+function App(props) {
+  const [usuario, setUsuario] = useState('');
+  
+  function handlePesquisa(){
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+    
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+    <>
+     <input Classname = 'usuarioInput'  placeholder='Usuário' value={usuario} onChange={e => setUsuario(e.target.value)}/>
+      <button type='button' onClick= {handlePesquisa}>Pesquisar</button>
+    </>
+     
   );
 }
 
