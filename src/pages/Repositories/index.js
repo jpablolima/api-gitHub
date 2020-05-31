f451b78f4ac4fb883e47ab814185bebc9e770c5b
João Pablo <jp.pablolima@gmail.com>
2020-05-31 18:49:50 -0300

    tratamento de erros

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import * as S from './styled';
+import { useHistory } from 'react-router-dom';
 
 
 export default function Repositories(){
+  const history = useHistory();
   const [repositories, setRepositories] = useState([]);
   
   useEffect(() => {
    let repositoriesName = localStorage.getItem('repositoriesName');
+   if(repositoriesName !== null){
     repositoriesName = JSON.parse(repositoriesName);
     setRepositories(repositoriesName);
-   // localStorage.clear();
+     localStorage.clear();
+   } else {
+     history.push('/');
+   }
 
   }, []);
 
